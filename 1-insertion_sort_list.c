bf0e6656fa9440dbb8094831ad886e84a383e7cb
Arweenn <arwendumont25@outlook.fr>
2024-01-17 15:53:05 +0100

    task 1 completed

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,5 +10,28 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *current, *temp;
 
+	for (current = (*list)->next; current; current = temp)
+	{
+		temp = current->next;
+
+		while (current->prev && current->prev->n > current->n)
+		{
+			current->prev->next = current->next;
+			if (current->next)
+				current->next->prev = current->prev;
+
+			current->next = current->prev;
+			current->prev = current->next->prev;
+			current->next->prev = current;
+
+			if (!current->prev)
+				*list = current;
+			else
+				current->prev->next = current;
+
+			print_list(*list);
+		}
+	}
 }
