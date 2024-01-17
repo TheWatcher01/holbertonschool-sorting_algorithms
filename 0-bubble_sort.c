a426473598f881cbac39edf9379ddc56bd92ce77
Arweenn <arwendumont25@outlook.fr>
2024-01-17 14:45:20 +0100

    task 0 completed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,5 +11,25 @@
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i;
+	int temp, swapped;
 
+	while (swapped)
+	{
+		swapped = 0;
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+
+				swapped = 1;
+				print_array(array, size); 
+			}
+		}
+		if (swapped == 0)
+			break;
+	}
 }
