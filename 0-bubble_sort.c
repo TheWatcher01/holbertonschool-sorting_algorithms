93a94934f641b4cd5645825352b47470f65c96ab
TheWatcher01 <teddydeberdt@gmail.com>
2024-01-18 12:03:56 +0100

    implement auxiliaries swap function in 0-bubble_sort.c file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,53 +1,62 @@
+/* Include header file */
 #include "sort.h"
 
 /**
- * bubble_sort - Function that sorts an array of integers in ascending order
- *               using the Bubble sort algorithm.
- * @array: Pointer to the array of integers to be sorted.
- * @size: The size of the array.
+ * swap - Swaps two elements in an array.
+ * @array: Array of integers.
+ * @i: Index of first element.
+ * @j: Index of second element.
+ */
+void swap(int *array, size_t i, size_t j)
+{
+	/* Store value at index i */
+	int temp = array[i];
+	/* Assign value at index j to index i */
+	array[i] = array[j];
+	/* Assign stored value to index j */
+	array[j] = temp;
+}
+
+/**
+ * bubble_sort - Sorts an array of integers using Bubble sort algorithm.
+ * @array: Array of integers to be sorted.
+ * @size: Size of array.
  *
- * Description: If the array is null or the size is less than 2, the function
- *              returns without making any changes. The function iterates over
- *              the array repeatedly, swapping adjacent elements if they are in
- *              wrong order. Iteration continues until no more swaps are made.
- *
- * Return: This function does not return a value.
+ * Description: If array is null or size is less than 2, function returns.
+ * Iterates over array, swapping adjacent elements if they are in wrong order.
+ * Iteration continues until no more swaps are made.
  */
 void bubble_sort(int *array, size_t size)
 {
-	/* Declare variables for the loop index, temporary storage, and swap flag */
+	/* Loop counter */
 	size_t i;
-	int temp, swapped;
+	/* Flag to check if swap occurred */
+	int swapped;
 
-	/* If the array is null or the size is less than 2, return */
+	/* Return if array is null or size is less than 2 */
 	if (!array || size < 2)
 		return;
 
 	/* Repeat until no more swaps are made */
-	do {
-		/* Reset the swapped flag */
+	do
+	{
+		/* Reset swapped flag */
 		swapped = 0;
 
-		/* Loop through the array */
+		/* Loop through array */
 		for (i = 0; i < size - 1; i++)
 		{
-			/* If the current element is greater than the next one */
+			/* If current element is greater than next one */
 			if (array[i] > array[i + 1])
 			{
-				/* Swap the elements */
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
-
-				/* Set the swapped flag to 1 */
+				/* Swap elements */
+				swap(array, i, i + 1);
+				/* Set swapped flag to 1 */
 				swapped = 1;
-
-				/* Print the array after the swap */
+				/* Print array after swap */
 				print_array(array, size);
 			}
 		}
 		/* Continue if a swap was made */
-	}
-
-	while (swapped);
+	} while (swapped);
 }
